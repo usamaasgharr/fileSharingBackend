5a863ef6832c9de240227e541af9c7db281f9a40
usamaasgharr <usama.asghar005@gmail.com>
2024-01-04 22:43:07 +0500

    initials

---
+++ database/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const MONGO_URI = `mongodb+srv://usama:Rprofessor@cluster.2dl6pvi.mongodb.net/fileShare?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
